c6d8a646557a05c4b667087d9f36883fa09c0af8
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-21 15:22:03 +0530

    code added to check equality between two lines

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
 public class LineComparison
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Line Comparison Problem Program");
+				//constants
+		int x1,y1,x2,y2;
+		int p1,q1,p2,q2;
+		
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter value of x1");
+		x1=s1.nextInt();
+		System.out.println("Enter value of y1");
+		y1=s1.nextInt();
+		System.out.println("Enter value of x2");
+		x2=s1.nextInt();
+		System.out.println("Enter value of y2");
+		y2=s1.nextInt();
+		
+		System.out.println("Enter value of p1");
+		p1=s1.nextInt();
+		System.out.println("Enter value of q1");
+		q1=s1.nextInt();
+		System.out.println("Enter value of p2");
+		p2=s1.nextInt();
+		System.out.println("Enter value of q2");
+		q2=s1.nextInt();
+		//computation
+		Double distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		Double distance2=Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+		System.out.println("The distance between 2 points of Line1 is "+distance);
+		System.out.println("The distance between 2 points of Line2 is "+distance2);
+		
+		//comparison using equals()
+		
+		if(distance.equals(distance2))
+		{
+			System.out.println("Lines are equal");
+		}
+		else
+			System.out.println("Lines are not equal");
+			
+		
 	}
 }
